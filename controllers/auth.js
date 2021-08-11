68afd77f59ca4ce291831c08e9b29bd6254cc977
Maximo Carrion <maximo.carrion.dev@gmail.com>
2021-08-10 21:48:13 -0500

    validaciones del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,28 @@ const Usuario = require('../models/Usuario');
 
 const crearUsuario =  async (req,res = response)=>{
 
-    
+    const { email, password }  = req.body;
+
+
     try {
-        const usuario = new Usuario(req.body);
+        let usuario = await  Usuario.findOne({email:email});
+
+        if ( usuario) {
+            return res.status(400).json({
+                ok:false,
+                msg:'Un usuario existe con ese correo'
+            });
+        }    
+
+
+        usuario = new Usuario(req.body);
 
         await usuario.save();
 
         res.status(201).json({
             ok:true,
-            msg:'registro'
+            uid: usuario.id,
+            name: usuario.name
         })
     } catch (error) {
         console.log(error);

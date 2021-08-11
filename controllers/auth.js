cfeae465be88780fe52289561f2890b5d899fbc3
Maximo Carrion <maximo.carrion.dev@gmail.com>
2021-08-10 22:10:17 -0500

    encriptacion de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
 const crearUsuario =  async (req,res = response)=>{
@@ -19,6 +19,9 @@ const crearUsuario =  async (req,res = response)=>{
 
 
         usuario = new Usuario(req.body);
+        
+        const salt = bcrypt.genSaltSync();
+        usuario.password= bcrypt.hashSync(password,salt); 
 
         await usuario.save();
 
